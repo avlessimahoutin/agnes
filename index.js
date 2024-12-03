98b96bb0fc704cfa91b202907b0ad0d94ecd06f3
avlessigroup <avlessijeanbaptiste1@gmail.com>
2024-12-03 18:27:28 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,431 @@
+$('.menup').hide();
+
+$('.corpsp1').show(500);
+$('.corpsp2').hide();
+$('.corpsp3').hide();
+$('.corpsp4').hide();
+$('.corpsp5').hide();
+$('.corpsp6').hide();
+$('.corpsp7').hide();
+$('.corpsp8').hide();
+$('.corpsp9').hide();
+
+
+$('.corpso1').show(500);
+$('.corpso2').hide();
+$('.corpso3').hide();
+$('.corpso4').hide();
+$('.corpso5').hide();
+$('.corpso6').hide();
+$('.corpso7').hide();
+$('.corpso8').hide();
+$('.corpso9').hide();
+
+
+$('.menu0').click(function () {
+    $('.menu0').hide();
+    $('.corpsp').hide();
+
+    $('.menup').show(500);
+
+})
+
+$('.menu1').click(function () {
+    $('.menup').hide(500);
+    $('.menu0').show();
+
+    $('.corpsp1').hide();
+    $('.corpsp2').hide();
+    //$('.corpsp3').hide();
+    $('.corpsp4').hide();
+    $('.corpsp5').hide();
+    // $('.corpsp6').hide();
+    $('.corpsp7').hide();
+    // $('.corpsp8').hide();
+    $('.corpsp9').hide();
+
+    $('.corpsp').show(500);
+
+
+
+    $('.corpsp1').show(500);
+
+})
+
+
+$('.menu8').click(function () {
+    $('.menup').hide(500);
+    $('.menu0').show();
+
+    $('.corpsp1').hide();
+    $('.corpsp2').hide();
+    // $('.corpsp3').hide();
+    $('.corpsp4').hide();
+    $('.corpsp5').hide();
+    // $('.corpsp6').hide();
+    $('.corpsp7').hide();
+    // $('.corpsp8').hide();
+    $('.corpsp9').hide();
+
+    $('.corpsp').show(500);
+
+
+
+    $('.corpsp8').show(500);
+
+})
+
+
+$('.menu9').click(function () {
+    $('.menup').hide(500);
+    $('.menu0').show();
+
+    $('.corpsp1').hide();
+    $('.corpsp2').hide();
+    // $('.corpsp3').hide();
+    $('.corpsp4').hide();
+    $('.corpsp5').hide();
+    //  $('.corpsp6').hide();
+    $('.corpsp7').hide();
+    //  $('.corpsp8').hide();
+    $('.corpsp9').hide();
+
+    $('.corpsp').show(500);
+
+
+
+    $('.corpsp9').show(500);
+
+})
+
+$('.menu2').click(function () {
+    $('.menup').hide(500);
+    $('.menu0').show();
+
+    $('.corpsp1').hide();
+    $('.corpsp2').hide();
+    //  $('.corpsp3').hide();
+    $('.corpsp4').hide();
+    $('.corpsp5').hide();
+    //  $('.corpsp6').hide();
+    $('.corpsp7').hide();
+    //  $('.corpsp8').hide();
+    $('.corpsp9').hide();
+
+    $('.corpsp').show(500);
+
+
+
+    $('.corpsp2').show(500);
+
+})
+
+$('.menu3').click(function () {
+    $('.menup').hide(500);
+    $('.menu0').show();
+
+    $('.corpsp1').hide();
+    $('.corpsp2').hide();
+    //  $('.corpsp3').hide();
+    $('.corpsp4').hide();
+    $('.corpsp5').hide();
+    //  $('.corpsp6').hide();
+    $('.corpsp7').hide();
+    //  $('.corpsp8').hide();
+    $('.corpsp9').hide();
+
+    $('.corpsp').show(500);
+
+
+
+    $('.corpsp3').show(500);
+
+})
+
+$('.menu4').click(function () {
+    $('.menup').hide(500);
+    $('.menu0').show();
+
+    $('.corpsp1').hide();
+    $('.corpsp2').hide();
+    //   $('.corpsp3').hide();
+    $('.corpsp4').hide();
+    $('.corpsp5').hide();
+    // $('.corpsp6').hide();
+    $('.corpsp7').hide();
+    // $('.corpsp8').hide();
+    $('.corpsp9').hide();
+
+    $('.corpsp').show(500);
+
+
+
+    $('.corpsp4').show(500);
+
+})
+
+$('.menu5').click(function () {
+    $('.menup').hide(500);
+    $('.menu0').show();
+
+    $('.corpsp1').hide();
+    $('.corpsp2').hide();
+    // $('.corpsp3').hide();
+    $('.corpsp4').hide();
+    $('.corpsp5').hide();
+    //  $('.corpsp6').hide();
+    $('.corpsp7').hide();
+    //   $('.corpsp8').hide();
+    $('.corpsp9').hide();
+
+    $('.corpsp').show(500);
+
+
+
+    $('.corpsp5').show(500);
+
+})
+
+$('.menu6').click(function () {
+    $('.menup').hide(500);
+    $('.menu0').show();
+
+    $('.corpsp1').hide();
+    $('.corpsp2').hide();
+    //  $('.corpsp3').hide();
+    $('.corpsp4').hide();
+    $('.corpsp5').hide();
+    // $('.corpsp6').hide();
+    $('.corpsp7').hide();
+    //$('.corpsp8').hide();
+    $('.corpsp9').hide();
+
+    $('.corpsp').show(500);
+
+
+
+    $('.corpsp6').show(500);
+
+})
+
+$('.menu7').click(function () {
+    $('.menup').hide(500);
+    $('.menu0').show();
+
+    $('.corpsp1').hide();
+    $('.corpsp2').hide();
+    //  $('.corpsp3').hide();
+    $('.corpsp4').hide();
+    $('.corpsp5').hide();
+    //  $('.corpsp6').hide();
+    $('.corpsp7').hide();
+    //  $('.corpsp8').hide();
+    $('.corpsp9').hide();
+
+    $('.corpsp').show(500);
+
+
+
+    $('.corpsp7').show(500);
+
+})
+
+
+
+////////////////////////////////////////////////////////////////////////////
+
+$('.menu01').click(function () {
+
+
+    $('.corpso1').hide();
+    $('.corpso2').hide();
+    //  $('.corpso3').hide();
+    $('.corpso4').hide();
+    $('.corpso5').hide();
+    //  $('.corpso6').hide();
+    $('.corpso7').hide();
+    //  $('.corpso8').hide();
+    $('.corpso9').hide();
+
+
+    $('.corpso1').show(500);
+
+})
+
+$('.menu02').click(function () {
+
+
+    $('.corpso1').hide();
+    $('.corpso2').hide();
+    // $('.corpso3').hide();
+    $('.corpso4').hide();
+    $('.corpso5').hide();
+    // $('.corpso6').hide();
+    $('.corpso7').hide();
+    //   $('.corpso8').hide();
+    $('.corpso9').hide();
+
+
+    $('.corpso2').show(500);
+
+})
+
+
+$('.menu03').click(function () {
+
+
+    $('.corpso1').hide();
+    $('.corpso2').hide();
+    //$('.corpso3').hide();
+    $('.corpso4').hide();
+    $('.corpso5').hide();
+    //   $('.corpso6').hide();
+    $('.corpso7').hide();
+    //  $('.corpso8').hide();
+    $('.corpso9').hide();
+
+
+    $('.corpso3').show(500);
+
+})
+
+
+$('.menu04').click(function () {
+
+
+    $('.corpso1').hide();
+    $('.corpso2').hide();
+    //  $('.corpso3').hide();
+    $('.corpso4').hide();
+    $('.corpso5').hide();
+    //  $('.corpso6').hide();
+    $('.corpso7').hide();
+    //  $('.corpso8').hide();
+    $('.corpso9').hide();
+
+
+    $('.corpso4').show(500);
+
+})
+
+
+$('.menu04').click(function () {
+
+
+    $('.corpso1').hide();
+    $('.corpso2').hide();
+    // $('.corpso3').hide();
+    $('.corpso4').hide();
+    $('.corpso5').hide();
+    //   $('.corpso6').hide();
+    $('.corpso7').hide();
+    //   $('.corpso8').hide();
+    $('.corpso9').hide();
+
+
+    $('.corpso4').show(500);
+
+})
+
+
+$('.menu06').click(function () {
+
+
+    $('.corpso1').hide();
+    $('.corpso2').hide();
+    //  $('.corpso3').hide();
+    $('.corpso4').hide();
+    $('.corpso5').hide();
+    //   $('.corpso6').hide();
+    $('.corpso7').hide();
+    //   $('.corpso8').hide();
+    $('.corpso9').hide();
+
+
+    $('.corpso6').show(500);
+
+})
+
+$('.menu07').click(function () {
+
+
+    $('.corpso1').hide();
+    $('.corpso2').hide();
+    // $('.corpso3').hide();
+    $('.corpso4').hide();
+    $('.corpso5').hide();
+    //   $('.corpso6').hide();
+    $('.corpso7').hide();
+    // $('.corpso8').hide();
+    $('.corpso9').hide();
+
+
+    $('.corpso7').show(500);
+
+})
+
+$('.menu08').click(function () {
+
+
+    $('.corpso1').hide();
+    $('.corpso2').hide();
+    //  $('.corpso3').hide();
+    $('.corpso4').hide();
+    $('.corpso5').hide();
+    //  $('.corpso6').hide();
+    $('.corpso7').hide();
+    // $('.corpso8').hide();
+    $('.corpso9').hide();
+
+
+    $('.corpso8').show(500);
+
+})
+
+$('.menu09').click(function () {
+
+
+    $('.corpso1').hide();
+    $('.corpso2').hide();
+    // $('.corpso3').hide();
+    $('.corpso4').hide();
+    $('.corpso5').hide();
+    // $('.corpso6').hide();
+    $('.corpso7').hide();
+    //  $('.corpso8').hide();
+    $('.corpso9').hide();
+
+
+    $('.corpso9').show(500);
+
+})
+
+$('.menu05').click(function () {
+
+
+    $('.corpso1').hide();
+    $('.corpso2').hide();
+    //  $('.corpso3').hide();
+    $('.corpso4').hide();
+    $('.corpso5').hide();
+    //   $('.corpso6').hide();
+    $('.corpso7').hide();
+    //   $('.corpso8').hide();
+    $('.corpso9').hide();
+
+
+    $('.corpso5').show(500);
+
+})
+$('.menu03').hide();
+$('.menu06').hide();
+$('.menu08').hide();
+$('.menu07').hide();
+
+
+$('.menu3').hide();
+$('.menu6').hide();
+$('.menu8').hide();
+$('.menu7').hide();
+
+
